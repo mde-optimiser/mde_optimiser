e5bc34d82728b3cdfc3f0b794438f04c8f641e2e
alxbrd <alex+git@onboot.org>
2017-10-13 01:59:59 +0100

    Fixed script errors

--- deploy/publish-artifacts.sh
+++ deploy/publish-artifacts.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+cd ../deploy/
 
-pwd
-
-ls -la
-
-openssl aes-256-cbc -K $EKEY -iv $EIV -in ../deploy/$DEPLOY_KEY -out ../deploy/deploy_key -d
-
-ssh-add ../deploy/deploy_key
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
 cd $TRAVIS_BUILD_DIR/..
 
