a7e8a3b9cd044c213fad959d296b64d03478497c
alxbrd <alex+git@onboot.org>
2017-10-13 02:33:08 +0100

    Fixed script errors

--- deploy/publish-artifacts.sh
+++ deploy/publish-artifacts.sh
@@ -7,7 +7,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-echo $ENCRYPTED_IV
+export
 
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $DEPLOY_KEY -out deploy_key -d
 chmod 600 deploy_key
