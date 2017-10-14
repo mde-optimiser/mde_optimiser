0376a40a0265c443f9b0e9adba7beef41f3ee770
alxbrd <alex+git@onboot.org>
2017-10-14 21:58:08 +0100

    Updated deploy keys

--- deploy/publish-artifacts.sh
+++ deploy/publish-artifacts.sh
@@ -14,11 +14,14 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $DEPLOY_KEY -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_keys.tar.enc -out deploy_keys.tar -d
 
-chmod 600 deploy_key
+tar xvf deploy_keys.tar
+
 eval `ssh-agent -s`
-ssh-add deploy_key
+
+chmod 600 deploy_keys/$DEPLOY_KEY
+ssh-add deploy_keys/$DEPLOY_KEY
 
 cd $TRAVIS_BUILD_DIR/..
 
