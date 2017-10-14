497c5cbcefe22a15b8e7f904406b02ddb05f989d
alxbrd <alex+git@onboot.org>
2017-10-14 23:26:35 +0100

    MDEO debugging

--- deploy/publish-artifacts.sh
+++ deploy/publish-artifacts.sh
@@ -14,6 +14,10 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
+pwd
+
+ls -la
+
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_keys.tar.enc -out deploy_keys.tar -d
 
 tar xvf deploy_keys.tar
