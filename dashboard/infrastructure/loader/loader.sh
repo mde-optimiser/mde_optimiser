92deadd2068e3eed4d8080384f58c8dabb2e3057
tamaram <tamara.manasieva@kcl.ac.uk>
2018-01-30 16:42:42 +0000

    [DockerFiles] Fix loader container, queue still not working.

---
+++ dashboard/infrastructure/loader/loader.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec java -classpath "/var/app/mdeo/uk.ac.kcl.mdeoptimise.dataloader/:/var/app/mdeo/uk.ac.kcl.mdeoptimise.dataloader.lib/*" uk.ac.kcl.mdeoptimise.dataloader.DataLoader
