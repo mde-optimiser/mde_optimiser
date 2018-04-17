18933929748543e3be673294c4417bf4fd49eb7b
tamaram <tamara.manasieva@kcl.ac.uk>
2018-04-17 09:11:32 +0100

    Fix Dockerfile for MDEO

--- dashboard/infrastructure/worker/mdeo.sh
+++ dashboard/infrastructure/worker/mdeo.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -classpath "/var/app/plugins/*:/var/app/mdeo/uk.ac.kcl.mdeoptimise/target/classes:/var/app/mdeo/uk.ac.kcl.mdeoptimise.lib/*:/var/app/mdeo/uk.ac.kcl.mdeoptimise.ui/target/classes:/var/app/mdeo/uk.ac.kcl.mdeoptimise.tests/target/classes:/var/app/mdeo/uk.ac.kcl.mdeoptimise.dashboard.api/target/classes" uk.ac.kcl.ui.launch.RunOptimisation /var/app/case_studies/cra/ /var/app/case_studies/cra/mutation-solution.mopt
+cd /var/app/mdeo && mvn clean compile
+
+export MDEO_CLASSPATH=`find ~/.m2 -name *.jar | python -c "import sys; print ':'.join(sys.stdin.read().splitlines())"`
+
+java -classpath "$MDEO_CLASSPATH:/var/app/mdeo/plugins/uk.ac.kcl.mdeoptimise/target/classes:/var/app/mdeo/plugins/uk.ac.kcl.mdeoptimise.ui/target/classes:/var/app/mdeo/plugins/uk.ac.kcl.mdeoptimise.tests/target/classes:/var/app/mdeo/dashboard/uk.ac.kcl.mdeoptimise.dashboard.api/target/classes" uk.ac.kcl.ui.launch.RunOptimisation /var/app/case_studies/cra/ /var/app/case_studies/cra/mutation-solution.mopt
