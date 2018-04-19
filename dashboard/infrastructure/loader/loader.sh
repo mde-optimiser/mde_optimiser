d138fd86a87a6a14d1a15db26c76496d1d07ec99
tamaram <tamara.manasieva@kcl.ac.uk>
2018-04-19 02:02:57 +0200

    [DataLoader] Convert to maven project & run via Docker

--- dashboard/infrastructure/loader/loader.sh
+++ dashboard/infrastructure/loader/loader.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-exec java -classpath "/var/app/mdeo/uk.ac.kcl.mdeoptimise.dataloader/:/var/app/mdeo/uk.ac.kcl.mdeoptimise.dataloader.lib/*" uk.ac.kcl.mdeoptimise.dataloader.DataLoader
+cd /var/app/loader && mvn clean compile
+
+export LOADER_CLASSPATH=`find ~/.m2 -name *.jar | python -c "import sys; print ':'.join(sys.stdin.read().splitlines())"`
+
+java -classpath "$LOADER_CLASSPATH:/var/app/loader/target/classes:/var/app/loader/lib/*" uk.ac.kcl.mdeoptimise.dataloader.DataLoader
