f1b3ab83387eb8a32c7e65f1afd4de5edf902611
tamaram <tamara.manasieva@kcl.ac.uk>
2018-04-22 21:25:03 +0200

    [WebApp] Dockerfile: start with Jetty

--- dashboard/infrastructure/web/web.sh
+++ dashboard/infrastructure/web/web.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Install maven 3.3.9
-RUN wget -O /tmp/apache-maven-3.3.9-bin.tar.gz http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz && \
+wget -O /tmp/apache-maven-3.3.9-bin.tar.gz http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz && \
     tar xzf /tmp/apache-maven-3.3.9-bin.tar.gz -C /opt/ && \
     ln -s /opt/apache-maven-3.3.9 /opt/maven && \
     ln -s /opt/maven/bin/mvn /usr/local/bin  && \
     rm -f /tmp/apache-maven-3.3.9-bin.tar.gz
 
-cd /var/app/web && mvn clean compile && mvn package
+cd /var/app/web && mvn clean dependency:copy-dependencies package
+cp /var/app/web/target/statusdashboard-1.0-SNAPSHOT.war /opt/jetty/webapps/
+
+java -Djetty.home=/opt/jetty -jar /opt/jetty/start.jar
