2a0b0c0723cd347e00dcf2bbc555d975f13a9ca1
tamaram <tamara.manasieva@kcl.ac.uk>
2018-04-24 17:47:24 +0100

    [MDEO] Modify way of sending messages (creating threads)

--- src/plugins/uk.ac.kcl.mdeoptimise/src/uk/ac/kcl/optimisation/moea/IntermediateSolutionsCollector.java
+++ src/plugins/uk.ac.kcl.mdeoptimise/src/uk/ac/kcl/optimisation/moea/IntermediateSolutionsCollector.java
@@ -1,10 +1,14 @@
 package uk.ac.kcl.optimisation.moea;
 
 import java.io.IOException;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import uk.ac.kcl.json.JsonEncoder;
 import uk.ac.kcl.mdeoptimise.Optimisation;
@@ -16,9 +20,12 @@ import uk.ac.kcl.mdeoptimise.dashboard.api.sender.Sender;
  * @author Tamara
  *
  */
-public class IntermediateSolutionsCollector {
-	
-	private Map<Integer, List<MoeaOptimisationSolution>> evolutionNumToSolutions = new HashMap<Integer, List<MoeaOptimisationSolution>>();
+public class IntermediateSolutionsCollector implements Runnable {
+
+	private Queue<Map.Entry<Integer, List<MoeaOptimisationSolution>>> solutionsPerEvolutionQueue =
+			new LinkedBlockingQueue<Map.Entry<Integer, List<MoeaOptimisationSolution>>>();
+	private Map<Integer, List<MoeaOptimisationSolution>> evolutionNumToSolutions =
+			new HashMap<Integer, List<MoeaOptimisationSolution>>();
 	private List<Thread> threads = new ArrayList<Thread>(); // collection of Threads that are sending messages to queue
 	private Sender sender = new Sender();
 	private Optimisation optimisationModel;
@@ -27,6 +34,8 @@ public class IntermediateSolutionsCollector {
 	private int totalEvolutions;
 	private int populationSize;
 	private int evolutionCounter = 1;
+	private final Map.Entry<Integer, List<MoeaOptimisationSolution>> DONE =
+			new AbstractMap.SimpleEntry<>(totalEvolutions, new ArrayList<MoeaOptimisationSolution>());
 
 	public IntermediateSolutionsCollector(Optimisation optimisationModel, String experimentId) {
 		this.totalEvolutions = optimisationModel.getOptimisation().getAlgorithmEvolutions();
@@ -43,15 +52,15 @@ public class IntermediateSolutionsCollector {
 	 */
 	void addIntermediateSolution(MoeaOptimisationSolution solution) {
 		numOfSolutions++;
-		
+
 		// Initialise the ArrayList for current evolution number.
 		if (!evolutionNumToSolutions.containsKey(evolutionCounter))
 			evolutionNumToSolutions.put(evolutionCounter, new ArrayList<>());
-		
+
 		// Add the given solution to the ArrayList for current evolution number.
 		evolutionNumToSolutions.get(evolutionCounter).add(solution);
 		System.out.println("Run number: " + evolutionCounter + ". Solutions found: " + evolutionNumToSolutions.get(evolutionCounter).size());
-		
+
 		checkEvolutionComplete();
 		checkExperimentComplete();
 	}
@@ -65,11 +74,12 @@ public class IntermediateSolutionsCollector {
 	 */
 	private void checkEvolutionComplete() {
 		if(numOfSolutions == populationSize && evolutionCounter != totalEvolutions) {
-			// Start a new Thread for the evolution that just finished.
-			Thread evolutionThread = new EvolutionThread(evolutionCounter);
-			evolutionThread.start();
-			threads.add(evolutionThread);
-			
+			solutionsPerEvolutionQueue.add(new AbstractMap.SimpleEntry<>(evolutionCounter, evolutionNumToSolutions.get(evolutionCounter)));
+
+			// Maximum 3 Threads should be started.
+			if (evolutionCounter < 4)
+				startThread();
+
 			// Start a new evolution and reset number of solutions counter.
 			System.out.println("[MDEO] Finished run number " + evolutionCounter);
 			evolutionCounter++;
@@ -83,42 +93,54 @@ public class IntermediateSolutionsCollector {
 	 */
 	private void checkExperimentComplete() {
 		if (evolutionCounter == totalEvolutions) {
+			solutionsPerEvolutionQueue.add(DONE);
+
 			threads.stream().forEach(thread -> {
 				try {
 					thread.join();
 				} catch (InterruptedException e) {
-					System.out.println("[MDEO] A thread sending a message to the queue for evolution number " + 
-							evolutionCounter + " has failed.");
+					System.out.println("[MDEO] A thread sending a message to the queue has failed.");
 				}
 			});
 		}
 	}
 
-	/**
-	 * Thread that sends a message to the queue containing intermediate solutions for a given evolution.
-	 */
-	private class EvolutionThread extends Thread {
+	private void startThread() {
+		Thread t = new Thread(this);
+		t.start();
+		threads.add(t);
+	}
 
-		int evolutionNumber;
-
-		public EvolutionThread(int evolutionNumber) {
-			this.evolutionNumber = evolutionNumber;
-		}
-
-		@Override
-		public void run() {
-			String  message = "";
+	// This will try to get elements from the queue constantly, and only stop when the message DONE is sent.
+	@Override
+	public void run() {
+		while (true) {
 			try {
-				message = JsonEncoder.generateIntermediateSolutionText(
-						optimisationModel, 
-						evolutionNumToSolutions.get(evolutionNumber).stream().iterator(), 
-						experimentId, 
-						evolutionNumber);
-			} catch (IOException e) {
-				e.printStackTrace();
+				Map.Entry<Integer, List<MoeaOptimisationSolution>> item = solutionsPerEvolutionQueue.remove();
+				if (item == DONE) {
+					// Add message DONE to the queue again, for other threads to see it. Stop the execution.
+					solutionsPerEvolutionQueue.add(DONE);
+					break;
+				}
+				// process `item`
+				int evolutionNumber = item.getKey();
+				System.out.println("[MDEO] processing intermediate solution for evolution number: " + evolutionNumber);
+				List<MoeaOptimisationSolution> solutions = item.getValue();
+				String  message = "";
+				try {
+					message = JsonEncoder.generateIntermediateSolutionText(
+							optimisationModel,
+							solutions.stream().iterator(),
+							experimentId,
+							evolutionNumber);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				sender.sendMessage(message);
+				System.out.println("[MDEO] intermediate solution message sent: " + message);
+			} catch (NoSuchElementException e) {
+				// Queue is empty at the moment.
 			}
-			sender.sendMessage(message);
-			System.out.println("[MDEO] intermediate solution message sent: " + message);
 		}
 	}
 }
