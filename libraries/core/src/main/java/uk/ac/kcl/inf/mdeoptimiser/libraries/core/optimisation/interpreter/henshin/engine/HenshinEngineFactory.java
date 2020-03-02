dafd02ef046d17cb23aa14f751373086a77e3ca9
alxbrd <alxbrd@users.noreply.github.com>
2020-03-02 23:52:39 +0000

    Added static loader for Henshin engines

---
+++ libraries/core/src/main/java/uk/ac/kcl/inf/mdeoptimiser/libraries/core/optimisation/interpreter/henshin/engine/HenshinEngineFactory.java
@@ -0,0 +1,37 @@
+package uk.ac.kcl.inf.mdeoptimiser.libraries.core.optimisation.interpreter.henshin.engine;
+
+import org.eclipse.emf.henshin.interpreter.Engine;
+import uk.ac.kcl.inf.mdeoptimiser.languages.mopt.SolverSpec;
+import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
+import at.ac.tuwien.big.momot.search.engine.MomotEngine;
+
+public class HenshinEngineFactory {
+	
+	private SolverSpec solverSpec;
+	private static Engine MOMOT_ENGINE;
+	private static Engine MDEO_ENGINE;
+	
+	static {
+		MOMOT_ENGINE = new MomotEngine();
+		MDEO_ENGINE = new EngineImpl();
+	}
+
+	
+	public HenshinEngineFactory(SolverSpec solverSpec){
+		this.solverSpec = solverSpec;
+	}
+	
+	/**
+	 * If a different Henshin Engine is configured, initialise it from the factory.
+	 * By default run the classic Henshin Engine.
+	 */
+	public Engine create() {
+		
+		switch(this.solverSpec.getSolverEngine()) {
+			case "momot":
+				return MOMOT_ENGINE;
+			default:
+				return MDEO_ENGINE;
+		}
+	}
+}
